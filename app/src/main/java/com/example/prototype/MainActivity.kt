3aad2606f89c3fc9b1a6b669eb2150164ad5b984
ShokiOkauchi <s15513@salesio-sp.ac.jp>
2019-06-28 12:09:53 +0900

    Outer frame

---
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.prototype
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.LinearLayout.*
+import android.graphics.Color
+import android.view.Gravity
+import android.widget.Button
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
