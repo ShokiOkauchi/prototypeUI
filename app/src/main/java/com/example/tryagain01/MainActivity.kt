a4d15598d3a0e36659c5a1b24ff7928d12512f55
ShokiOkauchi <s15513@salesio-sp.ac.jp>
2019-09-13 10:56:16 +0900

    kotlinでtab作成．

---
+++ app/src/main/java/com/example/tryagain01/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tryagain01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tabcontents:TabLayout=findViewById(R.id.tab_layout)
+        tabcontents.addTab(tabcontents.newTab().setText("Tab1"))
+        tabcontents.addTab(tabcontents.newTab().setText("Tab2"))
+        tabcontents.addTab(tabcontents.newTab().setText("Tab3"))
+    }
+
+}
