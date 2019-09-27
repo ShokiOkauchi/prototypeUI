a838be69d525ea5e8b9fff466b9bd28273b0ee69
ShokiOkauchi <s15513@salesio-sp.ac.jp>
2019-09-27 14:18:43 +0900

    動的エラー有りの途中経過プログラム(tablayout)

--- app/src/main/java/com/example/tryagain01/MainActivity.kt
+++ app/src/main/java/com/example/tryagain01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tryagain01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view_pager:ViewPager=findViewById(R.id.view_pager)
         val tabcontents:TabLayout=findViewById(R.id.tab_layout)
         tabcontents.addTab(tabcontents.newTab().setText("Tab1"))
         tabcontents.addTab(tabcontents.newTab().setText("Tab2"))
